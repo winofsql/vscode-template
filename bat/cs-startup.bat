445418f8562cb6db71c09ff384ebdd7f727d5658
sworc <lightbox@nifty.com>
2022-11-26 23:37:18 +0900

    Update cs-startup.bat

--- bat/cs-startup.bat
+++ bat/cs-startup.bat
@@ -1,12 +1,12 @@
 cd ..\ 
-mkdir form_01
-mkdir form_02
-mkdir form_03
-cd form_01
+mkdir cs-form_01
+mkdir cs-form_02
+mkdir cs-form_03
+cd cs-form_01
 cscript C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\download.vbs https://github.com/winofsql/cs-startup/raw/main/form_01 README.md
-cd ..\form_02
+cd ..\cs-form_02
 cscript C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\download.vbs https://github.com/winofsql/cs-startup/raw/main/form_02 README.md
-cd ..\form_03
+cd ..\cs-form_03
 cscript C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\download.vbs https://github.com/winofsql/cs-startup/raw/main/form_03 README.md
 
 cscript C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\workspace-build-download.vbs 2
